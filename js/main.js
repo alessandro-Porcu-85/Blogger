b8204091762fd99c913393fe2590d2bd6f7cf869
Alex <alessandro.biser@gmail.com>
2021-02-15 22:09:12 +0100

    carousel setting

--- js/main.js
+++ js/main.js
@@ -8,5 +8,31 @@ $(document).ready(function() {
         $nav.toggleClass('collapse');
     })
 
+    //Owl-carousel for blog
 
+
+    $(".owl-carousel").owlCarousel({
+        loop: true,
+        autoplay: false,
+        autoplayTimeout: 2000,
+        dots: false,
+        nav: true,
+        navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')],
+        responsiveClass: true,
+        responsive: {
+            0: {
+                items: 1,
+                nav: true
+            },
+            560: {
+                items: 2,
+                nav: false
+            },
+            960: {
+                items: 3,
+                nav: true,
+                loop: false
+            }
+        }
+    });
 });
