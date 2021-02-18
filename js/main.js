5dea7e57d669f513fd8012d43f17133096731a3e
Alex <alessandro.biser@gmail.com>
2021-02-18 14:37:25 +0100

    starting responsive

--- js/main.js
+++ js/main.js
@@ -39,7 +39,7 @@ $(document).ready(function() {
     $('.move-up span').click(function() {
         $('html, body').animate({
             scrollTop: 0
-        }, 1000);
+        }, 3000);
     })
 
 });
