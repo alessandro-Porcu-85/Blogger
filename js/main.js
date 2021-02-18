c0c1e126505489c67ef6498c2a79b42ace40e1f8
Alex <alessandro.biser@gmail.com>
2021-02-18 14:26:53 +0100

    styling footer + scroll top

--- js/main.js
+++ js/main.js
@@ -34,4 +34,12 @@ $(document).ready(function() {
             }
         }
     });
-})
+
+    // click to scroll top
+    $('.move-up span').click(function() {
+        $('html, body').animate({
+            scrollTop: 0
+        }, 1000);
+    })
+
+});
